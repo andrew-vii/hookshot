ad4a7c836ab40cd58066048db5182d4e3a2bd19a
Andrew <18285047+andrew-vii@users.noreply.github.com>
2020-05-10 14:02:46 -0400

    Create glaive.py

---
+++ glaive.py
@@ -0,0 +1,16 @@
+# HIBP API Integration
+
+import os
+import sys
+import requests
+
+
+def get_api_key(api_file):
+
+
+def check_account_breaches(accounts_file):
+
+def check_account_pastes(accounts_file): 
+
+
+
