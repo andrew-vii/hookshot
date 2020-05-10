b00352eabb7808e0d2f995e8041b38e31c7cf941
Andrew <18285047+andrew-vii@users.noreply.github.com>
2020-05-10 14:56:26 -0400

    Update glaive.py

--- glaive.py
+++ glaive.py
@@ -4,13 +4,27 @@ import os
 import sys
 import getopt 
 import requests
+import json
+import time
+import datetime
 
-
+# Open HIBP API Key file and read in API Key value 
 def get_api_key(api_file):
+  try:
+    g = open(api_file,"r")
+    api_key = g.readline().strip()
+  
+  except IOError:
+    print("Error opening HIBP API Key File")
+    exit()
+    
+  else:
+    print("Unknown Error")
+    exit() 
   
   return api_key
 
-def check_account_breaches(account):
+def submit_account_breaches(account):
   
   # Set up payload with our HIBP API key and distinctive UAS
   payload = {'hibp-api-key': get_api_key(api_key_file),
@@ -20,10 +34,21 @@ def check_account_breaches(account):
   # Set URL for account breaches
   breach_url = 'https://haveibeenpwned.com/api/v3/breachedaccount/' + account
   
-  # Submit our GET with rate limiting avoidance 
-  sleep(2) 
-  r = requests.get(submit_url, params=payload)
-  sleep(1) 
+  # Submit our GET request
+  breaches_response = requests.get(submit_url, params=payload)
+  
+  return breaches_response 
+  
+def submit_account_pastes(accounts_file): 
+  
+  return pastes_response
+
+def check_account_breaches(breach_response):
+  r = breach_response
+  breach_info = {
+    num_breaches: 0,
+    breaches: []    
+  }
   
   # Checking for breaches 
   if r.status_code == 404:
@@ -33,25 +58,18 @@ def check_account_breaches(account):
         print('Breach Check for: %s'%eml)
         num_breaches = len(data) 
         for d in data:
-            #   Simple info
+            #   We only really want the name and date of the breach
             breach = d['Name']
-            domain = d['Domain']
             breachDate = d['BreachDate']
-            sensitive = d['IsSensitive']
-            print('Account: %s\nBreach: %s\nSensitive: %s\nDomain: %s\nBreach Date:%s\n'%(eml,breach,sensitive,domain,breachDate))
-            #   or to print out the whole shebang comment above and uncomment below
-            #for k,v in d.items():
-            #    print(k+":"+str(v))
+            breach_info['breaches'].append(breach + " - " + breachDate)
+            
     else:
         data = r.json()
         print('Error: <%s>  %s'%(str(r.status_code),data['message']))
         exit()
-  
-  return breaches_response 
-  
-def check_account_pastes(accounts_file): 
-  
-  return pastes_response
+        
+  return breach_info
+
 
 
 def main(argv):
@@ -71,6 +89,8 @@ def main(argv):
       elif opt in ("-k", "--key"):
          api_key_file = arg
 
+          
+          
 if __name__ == "__main__":
    main(sys.argv[1:])
 
