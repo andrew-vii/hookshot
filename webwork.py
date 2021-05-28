87915d5b36fbef78f6c331a89ad3b7b0780722c3
Andrew <18285047+andrew-vii@users.noreply.github.com>
2021-05-28 19:14:27 -0400

    Update webwork.py

--- webwork.py
+++ webwork.py
@@ -79,8 +79,11 @@ def webscraper(URL):
   for i in url_list:
     print("Scraping " + i + "...)
     os.system("cewl -n -d 2 -e --email_file account_files/" + i + "_emails.txt " + i)
-    time.sleep(120)
-    print("Scraped " + i)
+    while (os.stat("account_files/" + i + "_emails.txt ").st_size < 1):
+          print("Waiting for scrape to complete...")
+          sleep(20)
+          
+    print("Scraped " + i + " !")
 
   return
     
