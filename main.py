8e5cb9e3f174559d15a656498566d77ca85976a0
andrew-vii <andrew.j.schoka@gmail.com>
2021-05-28 20:26:29 -0400

    Working state for webwork

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 # Main program control
 
 import time
@@ -9,33 +10,33 @@ import argparse
 import json
 import requests
 import hibp as hibp
-import breachalarm as ba
+import webwork as webwork
+
+
+# import hibp as hibp
+# import breachalarm as ba
 
 def main(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("hibp_keyfile", type=str, help="HIBP API Key File")
+    # parser.add_argument("ba_keyfile", type=str, help="HIBP API Key File")
+    parser.add_argument("URL", type=str, help="Target URL")
+    args = parser.parse_args()
 
-  parser = argparse.ArgumentParser()
-  parser.add_argument("hibp_keyfile", type=str, help="HIBP API Key File")
-  #parser.add_argument("ba_keyfile", type=str, help="HIBP API Key File")
-  parser.add_argument("URL", type=str, help="Target URL") 
-  args = parser.parse_args()
-  
-  while True: 
-    
-    # Run URL scraper
-    webwork.webscraper(args.URL)
-    
-    # Run HIBP routine 
-    hibp.hibp_checker(args.hibp_keyfile)
-    
-    # Run BreachAlarm routine - removed after BA removed API access
-    #ba.bral_checker(args.ba_keyfile, args.accountfile)
-    
-    # Set delay
-    time.sleep(10)
-  return 
-  
-  
-  
-# Run program
+    while True:
+        # Run URL scraper
+        webwork.webscraper(args.URL)
+
+        # Run HIBP routine
+        hibp.hibp_checker(args.hibp_keyfile)
+
+        # Run BreachAlarm routine - removed after BA removed API access
+        # ba.bral_checker(args.ba_keyfile, args.accountfile)
+
+        # Set delay
+        sleep(999)
+    return
+
+
 if __name__ == "__main__":
-  main(sys.argv)
+    main(sys.argv)
