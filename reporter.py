a90aadf2be630f9ded114319a4851af4daf198fe
Andrew <18285047+andrew-vii@users.noreply.github.com>
2021-06-07 13:39:50 -0400

    Create reporter.py

---
+++ reporter.py
@@ -0,0 +1,35 @@
+
+# FOR REFERENCE
+
+hibp.check(accounts)
+
+def hibp_checker(keyfile):
+  
+  
+  # Get accounts
+  account_dict = get_accounts()
+  
+  # Submit each account for pastes and breaches
+  for url, accounts in account_dict.items():
+
+      # Set up logfile
+    logfile = "output_files/" + url + "_accountlog" + str(datetime.datetime.now()) + ".log"
+    output_file = open(logfile,"a+")
+
+    for account in accounts:
+      time.sleep(1.5)
+      breach_result = submit_account_breaches(account, keyfile)
+      time.sleep(3)
+      paste_result = submit_account_pastes(account, keyfile)
+      time.sleep(1.5)
+
+      # Check results
+      breach_info = check_account_breaches(breach_result, account)
+      paste_info = check_account_pastes(paste_result, account)
+
+      # Output results to file
+      output_file.write("\n:::" + str(datetime.datetime.now()) + ":::\n:::" + account + ":::\n\n" + breach_info + "\n\n" + paste_info + "\n-----------------------\n\n")
+
+    output_file.close()
+  
+  return 
