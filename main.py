83b73c3795c7c62a2ae141bb6bb1e2e34196acb7
Andrew <18285047+andrew-vii@users.noreply.github.com>
2021-06-09 14:41:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -20,18 +20,22 @@ def main(argv):
     parser.add_argument("URL", type=str, help="Target URL")
     args = parser.parse_args()
 
-    while True:
-        # Run URL scraper
-        account_dict = webwork.webscraper(args.URL)
+    # Run URL scraper
+    account_dict = webwork.webscraper(args.URL)
 
-        # Run HIBP routine
-        main_dict = hibp.hibp_checker(args.hibp_keyfile, account_dict)
+    # Run HIBP routine
+    main_dict = hibp.hibp_checker(args.hibp_keyfile, account_dict)
         
-        # Run analysis
-        reporter.analyze(main_dict)
+    # Run analysis
+    analysis_dict = reporter.analyze(main_dict)
+        
+    # Produce report
+    reporter.report(analysis_dict)
 
-        # Set delay
-        time.sleep(1)
+    # Close 
+    print("\n----------------------------------------------")
+    print("PROGRAM COMPLETE")
+    print("------------------")
 
     return
 
