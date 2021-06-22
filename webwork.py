741ba5f2f13e9dfb9d044177e8578677c58d2513
andrew-vii <andrew.j.schoka@gmail.com>
2021-06-22 19:40:20 -0400

    Fixed subprocessing chopping commands

--- webwork.py
+++ webwork.py
@@ -14,13 +14,13 @@ import re
 # Check our input (single URL or file) 
 def check_input(url):
   if os.path.isfile(url):
-    print("Loaded - URL List Mode")
+    print("Loaded - URL List Mode\n")
     input_type = 2
   elif "http" or "www" or "://" in url:
-    print("Loaded - Single URL Mode")
+    print("Loaded - Single URL Mode\n")
     input_type = 1
   else:
-    print("URL Input Error")
+    print("URL Input Error\n")
     input_type = 0
     
   return input_type
@@ -43,14 +43,14 @@ def check_URL(URL, input_type):
       url_dict[URL] = check_response.status_code
 
   elif input_type == 2:
-    print("Checking list of URLs to reach")
+    print("Checking list of URLs to reach...")
     with open(URL) as f:
       url_list = f.readlines()
       url_dict = { url.strip() : 0 for url in url_list }
     for c in url_list:
       i = c.strip()
       time.sleep(0.5)
-      print("Checking " + i.strip() + "...")
+      print("\nChecking " + i.strip() + "...")
 
       # Clean up the url
       check_url = i.strip()
@@ -69,7 +69,7 @@ def check_URL(URL, input_type):
       print("Error reaching one or more URLs")
       status = 0
     else:
-      print("All " + str(len(url_list)) + " URLs Reachable!\n\n")
+      print("\nAll " + str(len(url_list)) + " URLs Reachable!\n")
       time.sleep(1)
       status = 1
       
@@ -114,17 +114,25 @@ def webscraper(URL):
     uas = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 
     # Run our scrapes in parallel -- best results with depth at 2 (faster) or 3 (longer, but more thorough)
-    scrape_command = "cewl %s --ua %s -n -d 2 -e --email_file %s" % (url_new, uas, output_file)
-    process_dict[i] = subprocess.Popen(scrape_command.split(), stdout=subprocess.PIPE)
+    # Set up scrape command
+    scrape_command = "cewl " + str(url_new) + " --ua '" + str(uas) + "' -n -d 3 -e --email_file " + str(output_file)
 
-  # Check states of subprocesses for completion
-  proc_states = {}
-  proc_complete = 0
+    # Run subprocess under our dict
+    process_dict[i] = subprocess.Popen(scrape_command, stdout=logvar, shell=True) #subprocess.PIPE)
+
+    # Debugging - check process status
+    print("Process Poll: " + str(process_dict[i].poll()))
+    time.sleep(1)
+
+  while True:
+    print("Process Poll: " + str(process_dict[i].poll()))
+    time.sleep(10)
 
   while proc_complete == 0:
     running_scrapes = 0
     for url in url_list:
       i = url.strip()
+      print("Process Poll: " + str(process_dict[i].poll()))
 
       if process_dict[i].poll() is None:
         proc_states[i] = 0
@@ -141,7 +149,6 @@ def webscraper(URL):
     else:
       proc_complete = 1
       print("Scrapes complete!")
-
 
   for url in url_list:
     
